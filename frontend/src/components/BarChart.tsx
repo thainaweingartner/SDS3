24c651192564574a271fcb954de9fb3718f87c71
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2021-05-09 20:28:44 -0300

    BarChart Integration

--- frontend/src/components/BarChart.tsx
+++ frontend/src/components/BarChart.tsx
@@ -1,12 +1,60 @@
-import { options, mockData } from '../assets/charts/BarChartConfig'; 
+import { options } from '../assets/charts/BarChartConfig'; 
 import Chart from 'react-apexcharts';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+
+type SeriesData = {
+  name: string;
+  data: number[];
+}
+
+type ChartData = {
+  labels: {
+    categories: string [];
+  };
+  series: SeriesData[];
+}
 
 const BarChart: React.FC = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels:{
+      categories:[]
+    }, 
+    series: [
+      {
+        name: '',
+        data: [],
+      }
+    ]
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+      .then(res => {
+        const data = res.data as SaleSuccess[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => round(100 * x.deals / x.visited, 1));
+        setChartData({ 
+          labels: {
+            categories: myLabels
+          }, 
+          series: [
+            {
+              name: '% Success',
+              data: mySeries
+            }
+          ] 
+        });
+      })
+  }, [])
 
   return (
     <Chart 
-      options={{...options, xaxis:mockData.labels}} 
-      series={mockData.series}
+      options={{...options, xaxis:chartData.labels}} 
+      series={chartData.series}
       type="bar"
       height="240"
     />
