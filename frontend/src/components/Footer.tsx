1900cf697ca3813cac93a85b0b17a1c1d9dfe2d5
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2021-05-09 21:10:40 -0300

    Fixing build Error

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+        <p className="text-light">App desenvolvido por <a href="https://github.com/thainaweingartner" target="_blank" rel="noreferrer">Thainá Weingartner Chagas</a></p>
         <p className="text-light"><small><strong>Semana Spring React</strong><br/>
           Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
